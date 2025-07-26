44a907105bf8e1ee1e647f2274fff4a7b20445d5
Ger Hobbelt <ger@hobbelt.com>
2025-07-26 19:18:01 +0200

    - fix warning C4263: 'void Composite::remove(const unsigned int)': member function does not override any base class virtual member function - fix warning C4264: 'void Component::remove(int)': no override available for virtual member function from base 'Component'; function is hidden

--- composite/Composite.cpp
+++ composite/Composite.cpp
@@ -21,13 +21,13 @@ class Component
 public:
   virtual ~Component() {}
   
-  virtual Component *getChild( int )
+  virtual Component *getChild( const unsigned int )
   {
     return 0;
   }
   
   virtual void add( Component * ) { /* ... */ }
-  virtual void remove( int ) { /* ... */ }
+  virtual void remove( const unsigned int ) { /* ... */ }
   
   virtual void operation() = 0;
 };
